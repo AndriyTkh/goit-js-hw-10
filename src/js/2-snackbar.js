1d38a680e6f3c006dea2bdf5da5ee0a9b03c8a43
AndriyTkh <151773877+AndriyTkh@users.noreply.github.com>
2024-05-07 09:31:36 +0300

    ver a2.0

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -4,3 +4,11 @@
 import iziToast from 'izitoast';
 // Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(form.elements.state.value);
+  console.log(form.elements.delay.value);
+});
