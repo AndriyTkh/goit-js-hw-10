9e97a31c4109ee058e3cf78eb18df91e1ff7409e
AndriyTkh <151773877+AndriyTkh@users.noreply.github.com>
2024-05-07 06:31:59 +0000

    Deploying to gh-pages from @ AndriyTkh/goit-js-hw-10@1d38a680e6f3c006dea2bdf5da5ee0a9b03c8a43 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import"./assets/vendor-d07556bb.js";const e=document.querySelector(".form");e.addEventListener("submit",t=>{t.preventDefault(),console.log(e.elements.state.value),console.log(e.elements.delay.value)});
+//# sourceMappingURL=commonHelpers2.js.map
